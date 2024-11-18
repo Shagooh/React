1cb18e7251efe510cd83bbad667bd8a636f279f0
Shagooh <stgorodu@gmail.com>
2024-11-17 23:02:37 -0300

    first commit

---
+++ projects/first/src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+
+import { TwitterFollowCard } from "./TwitterFollowCard"
+
+const users = [
+	{
+		userName: 'shagooh',
+		name: 'santi',
+		isFollowing: true
+	},
+	{
+		userName: 'carli',
+		name: 'protoss',
+		isFollowing: true
+	}
+]
+export function App() {
+	return (
+		<section className='App'>
+			{
+				users.map(({ userName, name, isFollowing }) => (
+					<TwitterFollowCard
+						key={userName}
+						userName={userName}
+						initialIsFollowing={isFollowing}
+					> 
+					{name}
+					</TwitterFollowCard>
+					
+				))
+
+			}
+
+		</section>
+
+	)
+}
